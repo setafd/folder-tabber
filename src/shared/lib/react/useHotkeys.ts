15ae4fadffe1abf04e7b36980f3af97629437cd2
Igor Gorin <igorgor.ru@gmail.com>
2025-11-25 11:44:42 +0300

    add hotkey ignore tags

--- src/shared/lib/react/useHotkeys.ts
+++ src/shared/lib/react/useHotkeys.ts
@@ -1,9 +1,23 @@
 import { useEffect, useRef } from 'react';
 
+const shouldFireEvent = (event: KeyboardEvent, tagsToIgnore: string[], triggerOnContentEditable = false) => {
+  if (event.target instanceof HTMLElement) {
+    if (triggerOnContentEditable) {
+      return !tagsToIgnore.includes(event.target.tagName);
+    }
+
+    return !event.target.isContentEditable && !tagsToIgnore.includes(event.target.tagName);
+  }
+
+  return true;
+};
+
 export const useHotkeys = <const T extends readonly string[]>(
   keys: T,
   callback: (key: T[number]) => void,
-  preventDefault = true
+  preventDefault = true,
+  tagsToIgnore: string[] = ['INPUT', 'TEXTAREA', 'SELECT'],
+  triggerOnContentEditable = false,
 ) => {
   const callbackRef = useRef(callback);
 
@@ -13,7 +27,7 @@ export const useHotkeys = <const T extends readonly string[]>(
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (keys.includes(event.key)) {
+      if (keys.includes(event.key) && shouldFireEvent(event, tagsToIgnore, triggerOnContentEditable)) {
         if (preventDefault) event.preventDefault();
         callbackRef.current(event.key);
       }
@@ -21,5 +35,5 @@ export const useHotkeys = <const T extends readonly string[]>(
 
     document.addEventListener('keydown', handleKeyDown);
     return () => document.removeEventListener('keydown', handleKeyDown);
-  }, [keys, preventDefault]);
+  }, [keys, preventDefault, tagsToIgnore, triggerOnContentEditable]);
 };
