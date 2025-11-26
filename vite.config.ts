80fe3343ee6bc5e98a36b7b727f190d7e8bcab48
Igor Gorin <igorgor.ru@gmail.com>
2025-11-26 16:48:12 +0300

    update configs && re-generate deps graph

--- vite.config.ts
+++ vite.config.ts
@@ -39,14 +39,4 @@ export default defineConfig({
       }
     }
   },
-  resolve: {
-    alias: {
-      '@app': '/src/app',
-      '@pages': '/src/pages',
-      '@widgets': '/src/widgets',
-      '@features': '/src/features',
-      '@entities': '/src/entities',
-      '@shared': '/src/shared',
-    },
-  },
 });
